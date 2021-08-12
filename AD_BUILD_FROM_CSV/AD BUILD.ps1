72df700197cf9ed03d081c20de24727c88741dba
Devin Bell <devin.a.bell@outlook.com>
2021-08-12 21:04:38 +0000

    update domain suffix

--- AD_BUILD_FROM_CSV/AD BUILD.ps1
+++ AD_BUILD_FROM_CSV/AD BUILD.ps1
@@ -13,10 +13,10 @@ New-ADUser -name $_.name -GivenName $_.givenname -Surname $_.surname -StreetAddr
  -PostalCode $_.zipcode -SamAccountName $_.username -OtherAttributes @{'title' = $_.occupation; 'carLicense' = $_.vehicle ; 'Middlename' = $_.middleinitial}
 
 #SET ACCOUNT PASSWORD FOR EACH USER
-Set-ADAccountPassword -Identity (((get-aduser -filter * -Properties *) | where name -eq $_.name) | select distinguishedname) -OldPassword $null -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $_.password -force) 
+Set-ADAccountPassword -Identity (((get-aduser -filter * -Properties *) | Where name -eq $_.name) | select distinguishedname) -OldPassword $null -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $_.password -force) 
 
 #ENABLE EACH ACCOUNT FOR USE
-Enable-ADAccount -Identity (((get-aduser -filter * -Properties *) | where name -eq $_.name) | select distinguishedname) }
+Enable-ADAccount -Identity (((get-aduser -filter * -Properties *) | Where name -eq $_.name) | select distinguishedname) }
 
 <#IF STATEMENT TO CREATE OU IF NOT ALREADY CREATED..
 OR MOVE ADUSER INTO THE OU ORGANIZED BY STATE#>
@@ -25,18 +25,16 @@ $user=get-aduser -filter * -Properties state
 
 #FOREACH TO CHECK IF EACH USER MATCHES AN OU AND IF SO MOVE IT IF NOT CREATE IT THEN MOVE IT
 $user | ForEach-Object -process {if($_.state -eq $null) {Write-Host "Not Moving" $_.Name" account into an OU"} 
-                                 else{if(Get-ADOrganizationalUnit -filter * | where name -eq $_.state) {Move-ADObject -Identity $_.distinguishedname `
-                                        -TargetPath ((Get-ADOrganizationalUnit -filter *) | where name -eq $_.state) | select distinguishedname } 
-                                      else{New-ADOrganizationalUnit -name $_.state -Path "DC=BELL,DC=LAN" -ProtectedFromAccidentalDeletion $false;
-                                           if(Get-ADOrganizationalUnit -filter * | where name -eq $_.state) {Move-ADObject -Identity $_.distinguishedname `
-                                              -TargetPath ((Get-ADOrganizationalUnit -filter *) | where name -eq $_.state) | select distinguishedname } 
-                                           } 
+                                 else{if(Get-ADOrganizationalUnit -filter * | where name -eq $_.state) {Move-ADObject -Identity $_.distinguishedname -TargetPath ((Get-ADOrganizationalUnit -filter *) | where name -eq $_.state) | select distinguishedname } 
+                                      else{New-ADOrganizationalUnit -name $_.state -Path "DC=BELL,DC=LOCAL" -ProtectedFromAccidentalDeletion $false;
+                                           if(Get-ADOrganizationalUnit -filter * | where name -eq $_.state) {Move-ADObject -Identity $_.distinguishedname -TargetPath ((Get-ADOrganizationalUnit -filter *) | where name -eq $_.state) | select distinguishedname } 
+                                           }
                                       }
                                 }
 #CREATE A DISTRO GROUP IN EACH OU AND ALL MEMBERS OF THE OU TO THE GROUP
 $user | ForEach-Object -process {if($_.state -eq $null) {Write-Host "Not Moving" $_.Name" account into a group"} 
                                  else{if(Get-ADGroup -filter * | where name -eq $_.state) {Move-ADObject -Identity $_.distinguishedname -TargetPath ((Get-ADgroup -filter *) | where name -eq $_.state) | select distinguishedname } 
-                                      else{New-ADgroup -name $_.state -Path "DC=BELL,DC=LAN" -GroupScope Global -GroupCategory Distribution;
+                                      else{New-ADgroup -name $_.state -Path "DC=BELL,DC=LOCAL" -GroupScope Global -GroupCategory Distribution;
                                            if(Get-ADgroup -filter * | where name -eq $_.state) {Move-ADObject -Identity $_.distinguishedname -TargetPath ((Get-ADgroup -filter *) | where name -eq $_.state) | select distinguishedname } 
                                            else{write-host "Not Moving" $_.Name" account into a group"}
                                            } 
