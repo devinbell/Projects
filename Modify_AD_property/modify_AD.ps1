b0d41fe63d785541846f041c469db5e657a16f14
Devin Bell <devin.a.bell@outlook.com>
2021-08-18 15:52:37 -0500

    do-while loop for input validation

--- Modify_AD_property/modify_AD.ps1
+++ Modify_AD_property/modify_AD.ps1
@@ -2,21 +2,28 @@
 This script will update a user's properties in active
 directory 
  #>
+do {
+    #get name of user changing
+     Write-Host "What is the username of the user you want to modify?"
+     $username = Read-Host
 
- #get name of user changing
- Write-Host "What is the username of the user you want to modify?"
- $username = Read-Host
+     #parameter for user that matches input 
+     $user = Get-ADUser -filter * -Properties * | where -Property UserPrincipalName -like $username
 
- #parameter for user that matches input 
- $user = Get-ADUser -filter * -Properties * | where -Property UserPrincipalName -like $username
+     #keep repeating until correct username is given
+} while ($null -eq $user)
  
- #get new information
- Write-Host "What is the new last name?"
- $last = Read-Host 
+ 
+ #update user AD profile
  if($user){
+      #get new information
+     Write-Host "What is the new last name?"
+     $last = Read-Host 
      $FirstName = $user.GivenName
-    Set-ADUser -Identity $user.distinguishedname -Surname $last -displayName "$FirstName $last"
-    Write-Information 'Last name updated successfully.'
+     
+     Set-ADUser -Identity $user.distinguishedname -Surname $last -displayName "$FirstName $last" -
+     
+     Write-Information 'Last name updated successfully.'
  }
 
 
