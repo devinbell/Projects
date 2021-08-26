93f023457027ac685f7778434665f764fd8ee5b7
DevinBell <devin.a.bell@outlook.com>
2021-08-26 13:33:19 -0500

    updated script

--- open MSSA Daily Apps/OpenFavApps.ps1
+++ open MSSA Daily Apps/OpenFavApps.ps1
@@ -1,17 +1,18 @@
-﻿start-process 'https://linkedin.com/'
+﻿start-process 'https://linkedin.com/' -WindowStyle Maximized
 Start-Process 'https://github.com/devinbell/Projects'
-Start-Process 'https://alh.learnondemand.net/User/Login?ReturnUrl=%2f'
-Start-Process 'https://www.clearancejobs.com/dashboard/your-profile'
+Start-Process 'https://alh.learnondemand.net/User/Login?ReturnUrl=%2f' 
+Start-Process 'https://www.clearancejobs.com/dashboard/your-profile' 
 Start-Process 'https://www.skillpipe.com/#/bookshelf/books'
-Start-Process 'https://msftmssa-my.sharepoint.com/'
-Start-Process -File "$($env:USERProfile)\AppData\Local\Microsoft\Teams\Update.exe" -ArgumentList '--processStart "Teams.exe"'
-invoke-item -Path "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+Start-Process 'https://msftmssa-my.sharepoint.com/' 
+Start-Process 'https://huntr.co/track/boards/612674a6b3c6fa0028cbbfba/board' 
+Start-Process -File "$($env:USERProfile)\AppData\Local\Microsoft\Teams\Update.exe" -ArgumentList '--processStart "Teams.exe"' -WindowStyle Maximized
+invoke-item -Path "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" 
 
 # SIG # Begin signature block
 # MIIF1gYJKoZIhvcNAQcCoIIFxzCCBcMCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU5NRE/1h+NWe6U+uWE2W1oovc
-# eSagggNOMIIDSjCCAjKgAwIBAgIQePhWFiGdWL5GuhWXo9HWlzANBgkqhkiG9w0B
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUjYrbUcZvyxbiGQ0cH1gC2sIv
+# iougggNOMIIDSjCCAjKgAwIBAgIQePhWFiGdWL5GuhWXo9HWlzANBgkqhkiG9w0B
 # AQsFADA9MRMwEQYDVQQDDApEZXZpbiBCZWxsMSYwJAYJKoZIhvcNAQkBFhdkZXZp
 # bmJlbGwyMDEzQHlhaG9vLmNvbTAeFw0yMTA3MjUxNDExNTlaFw0zMTAxMDEwNTAw
 # MDBaMD0xEzARBgNVBAMMCkRldmluIEJlbGwxJjAkBgkqhkiG9w0BCQEWF2Rldmlu
@@ -33,11 +34,11 @@ invoke-item -Path "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
 # bGwyMDEzQHlhaG9vLmNvbQIQePhWFiGdWL5GuhWXo9HWlzAJBgUrDgMCGgUAoHgw
 # GAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGC
 # NwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQx
-# FgQUH+eolvY6iqqTtfkVn2LnOJ/C8a0wDQYJKoZIhvcNAQEBBQAEggEATo4i4Scq
-# JhbxS6PBkjD47OfcBIjPfFe4aD9T0h/gfL3wxOTp8uoGkQ+4oYgS4DakMQTauda4
-# qRBTvJfZ5zxkqI1gSxylKJv5JuoO5FLIztkfET5e1Lt2nvM/NdKvquKpcP8MFLto
-# jt1jx0gyvxbR12M3SrB14BNhzoi0UlYZmUA4tmqmJiXJM3ALKXvq7/N9eDdBs3Lt
-# hToo80DLmggbfImngC/LTDllClrCSAXBjgm4KuS/kGpePnApLUeMP29KroAn6vJv
-# s1Rlg7anU3LAMPez+GIe0Gl/P+z6W4vDhvr3CRqIvGLbcg6C/z5LoGK00eE6o48e
-# mPCzBpymvPDOSQ==
+# FgQUbopx4y2vktGLW+2PE4amYSkTUq0wDQYJKoZIhvcNAQEBBQAEggEApnA/0Bte
+# 8H3xmLPCqxyZPTKqfWtpIGdJgt9WwdDmJ89PYQizjMo9vS69mhY++TDQWMSJEd36
+# dTc6ln9ckJBITgnyW2ws64ToE043+RnNlD6i0d8w9epsbMctuyCokeijvDe1JcIZ
+# HJiZwXx34IcVBpALhMN1dc9D/zXvwiCJtbc6/iW3lGq5gYjcSUF7L0DhVwPz6jpR
+# l8GpkavVtWQ9J5AeoKciSoF/tOjcsU/fPY24761SfsZkrUQhhsL9saIQyvsxC1CU
+# BvmmU810sKq3XneDRemq5hx3gWH5BbS5ZJTwWV5Dyl/ulWOpA1jV47HHrTKcHGHt
+# UqmcNi2EJFdEOA==
 # SIG # End signature block
