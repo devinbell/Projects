269d3c1e0705d7f9d27bbb3afa20a8440adce2bb
Devin Bell <devin.a.bell@outlook.com>
2021-08-31 14:31:43 -0500

    start-azvm and params

--- Azure_ARM/azCMD.ps1
+++ Azure_ARM/azCMD.ps1
@@ -1,13 +1,14 @@
 #this file will be used for practicing basic azure CL
 
 #connect account will open broswer to log into azure account 
-Connect-AzAccount
+Connect-AzAccount 
 
 #starts VM scale set 
 Start-AzVmss -VMScaleSetName -ResourceGroupName
+start-azvm -Name -ResourceGroupName
 
 #returns all VMs in the resource group
-Get-AzVM -ResourceGroupName
+Get-AzVM -ResourceGroupName -name
 
 #new AZVMSS    MANDO parameters
 new-azvmss -ResourceGroupName -VMScaleSetName
