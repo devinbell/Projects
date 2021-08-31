dacd7c4e7e8e3de4590fc28ef1ec64f915ec34f3
Devin Bell <devin.a.bell@outlook.com>
2021-08-31 14:33:12 -0500

    reorganize code

--- Azure_ARM/azCMD.ps1
+++ Azure_ARM/azCMD.ps1
@@ -3,6 +3,10 @@
 #connect account will open broswer to log into azure account 
 Connect-AzAccount 
 
+#new AZVMSS    MANDO parameters
+new-azvmss -ResourceGroupName -VMScaleSetName
+new-azvm -ResourceGroupName -Name
+
 #starts VM scale set 
 Start-AzVmss -VMScaleSetName -ResourceGroupName
 start-azvm -Name -ResourceGroupName
@@ -10,7 +14,5 @@ start-azvm -Name -ResourceGroupName
 #returns all VMs in the resource group
 Get-AzVM -ResourceGroupName -name
 
-#new AZVMSS    MANDO parameters
-new-azvmss -ResourceGroupName -VMScaleSetName
-new-azvm -ResourceGroupName -Name
+
 
