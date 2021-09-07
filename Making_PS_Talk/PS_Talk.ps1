72ee3d64ff87ada73421b74a6a535470a17d6fdf
Devin Bell <devin.a.bell@outlook.com>
2021-09-07 13:30:36 -0500

    increase voice rate

--- Making_PS_Talk/PS_Talk.ps1
+++ Making_PS_Talk/PS_Talk.ps1
@@ -3,12 +3,16 @@
 #get-module for making PS talk
 Get-Module Microsoft.PowerShell.Utility
 
-#new type of object 
+#create new type of object 
 Add-Type -AssemblyName System.speech 
 
 #create a new object speech synthesiser 
-$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer 
 
 #test
 $speak.speak{"Hello, World"}
 
+#change voice rate
+$speak.Rate=2
+
+#change voice to female voice
