083bc0b40000658252020b24ee8cdd9eb3031a64
DevinBell <devin.a.bell@outlook.com>
2021-09-13 13:55:16 -0500

    rename

---
+++ C++/hello world.cpp
@@ -0,0 +1,9 @@
+//this will be a basic code that writes hello world on the console
+#include <iostream>
+using namespace std;
+//main method
+int main(){
+    cout << "Hello World";
+
+    return 0
+}
