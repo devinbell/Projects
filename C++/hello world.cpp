d40a93a81b19bf39ee71b5cff73e0154b04df591
DevinBell <devin.a.bell@outlook.com>
2021-09-14 12:06:40 -0500

    header file

--- C++/hello world.cpp
+++ C++/hello world.cpp
@@ -1,9 +1,10 @@
 //this will be a basic code that writes hello world on the console
 #include <iostream>
 using namespace std;
+
 //main method
 int main(){
     cout << "Hello World";
 
-    return 0
+    return 0;
 }
