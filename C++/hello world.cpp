72e216578388db2e43d5c6f406bae4fd1eef7223
DevinBell <devin.a.bell@outlook.com>
2021-09-15 10:09:03 -0500

    commented for loop

--- C++/hello world.cpp
+++ C++/hello world.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 //main method
 int main(){
-
+    /*for loop that starts integer i at 0 and increments it
+    until it equals three*/ 
     for(int i=0; i<=3; i++){
         cout << i <<" Hello World" << endl ;
     }
