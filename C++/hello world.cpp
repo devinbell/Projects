602d53fbf0efae1f170db8ce8dda321f1c6ab170
DevinBell <devin.a.bell@outlook.com>
2021-09-23 14:47:21 -0500

    commenting

--- C++/hello world.cpp
+++ C++/hello world.cpp
@@ -4,22 +4,19 @@
 
 using namespace std;
 
-//method to calculate the square
+//method to calculate the square (method prototype required if defined under main())
 void square(int base){
 
-    cout<< "you entered " << base << " and that number squared is " << pow(base, 2); 
+    cout<< "you entered " << base << " and that number squared is " << pow(base, 2) << endl; 
 }
 
 //main method
 int main(){
     /*for loop that starts integer i at 0 and increments it
     until it equals three*/ 
-    for(int i=0; i<=3; i++){
-        cout << i <<" Hello World" << endl ;
+    for(int i=0; i<=5; i++){
+       square(i) ; //call the method 
     }
-
-    //test square method
-    square(12);
-   
+    cout << "Hello World!";
     return 0;
 }
