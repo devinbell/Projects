03f757146c542ab9005e4bd36560391f028ee30a
DevinBell <devin.a.bell@outlook.com>
2021-10-12 14:22:15 -0500

    scans event log for warnings

--- eventlog_scan_report/scan.ps1
+++ eventlog_scan_report/scan.ps1
@@ -4,5 +4,20 @@ results
 #>
 
 #get the event log from the last week and scan entries
-$eventlog = Get-Eventlog -LogName System -Before (Get-Date) -after ((get-date).AddDays(-7))
+$eventlog = Get-Eventlog -LogName Application -Before (Get-Date) -after ((get-date).AddDays(-7))
 
+#foreach loop to scan the event log
+ForEach($event in $eventlog){
+
+    if($event.EntryType -eq "WARNING"){
+        write-host "ERROR: $($event.Source) had an error, logging the error" -ForegroundColor Red
+        
+        $event | Out-File -FilePath C:\temp\logErrors.txt -NoClobber -Append
+        
+    }
+    else{
+
+        write-host "no issues found with $($event.Source)!" -ForegroundColor Green
+    }
+
+}
