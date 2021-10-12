cd575de11bea0eea073b5e3311ab37951d30a5ff
DevinBell <devin.a.bell@outlook.com>
2021-10-12 13:31:07 -0500

    get the eventlog

--- eventlog_scan_report/scan.ps1
+++ eventlog_scan_report/scan.ps1
@@ -3,6 +3,6 @@ This script will scan a event log, react to an error, log
 results
 #>
 
-#get the event log and scan entries
-$eventlog = Get-Eventlog -LogName System
+#get the event log from the last week and scan entries
+$eventlog = Get-Eventlog -LogName System -Before (Get-Date) -after ((get-date).AddDays(-7))
 
