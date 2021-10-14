d50f4fd0ca43c807ede8fff4405b69321312dc37
DevinBell <devin.a.bell@outlook.com>
2021-10-14 10:07:03 -0500

    update comment

--- remove_duplicate_modules/Remove_duplicate_modules.ps1
+++ remove_duplicate_modules/Remove_duplicate_modules.ps1
@@ -9,7 +9,7 @@ foreach ($Module in $modules)
   $latest = get-installedmodule $Module.name
   $specificmods = get-installedmodule $Module.name -allversions
   
-  # $(variable.property) returns the count method 
+  # $(variable.property) returns the value of the property 
   write-information -MessageData "$($specificmods.count) versions of $($Module.name) module found " -InformationAction Continue
   
  #compare each version of the module
